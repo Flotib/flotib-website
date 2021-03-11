d1657b7e3ac0577226d7b049c98378d779b0988f
Flotib <flotib@hotmail.fr>
2021-03-11 15:47:18 +0100

    Add a script to preload the images

---
+++ scripts/images-preload.js
@@ -0,0 +1,8 @@
+var pic = new Image();
+var pic2 = new Image();
+var pic3 = new Image();
+var pic4 = new Image();
+pic.src="../img/section/1.jpg";
+pic2.src="../img/section/2.jpg";
+pic3.src="../img/section/3.jpg";
+pic4.src="../img/section/4.jpg";
